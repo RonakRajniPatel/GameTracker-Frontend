db2a437875a3d2a35c9e9890ec7155a4b507701a
ronakpatel <ronakrajnipatel@gmail.com>
2023-07-03 01:39:26 -0400

    mern stack setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 function App() {
-
-  const [backendData, setBackendData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
-
   return (
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        backendData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
-    </div>
+    <div>hello world client page</div>
   )
 }
 
