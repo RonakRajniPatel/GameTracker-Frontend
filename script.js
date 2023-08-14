dc9834d09d8eb19048e9f02b219801171efd04ca
ronakpatel <ronakrajnipatel@gmail.com>
2023-08-14 05:32:33 -0400

    hours completed

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Define the Game class
 class Game {
-    constructor(title, status) {
+    constructor(title, status, hours) {
         this.title = title;
         this.status = status;
+        this.hours = hours;
     }
+
 }
 
 function saveGame() {
@@ -13,14 +15,22 @@ function saveGame() {
     const userStatus = document.querySelector('input[name="status"]:checked');
     const userStatusText = userStatus.value;
 
-    var myGame = new Game(userTitle, userStatusText);
+    var userHours = document.getElementById("hoursText").value;
+
+    var myGame = new Game(userTitle, userStatusText, userHours);
+
     gameList.push(myGame);
     console.log(gameList);
     gameInfoElement.innerHTML = `Title: ${myGame.title} <br> 
-                                 Status: ${myGame.status} <br>`;
+                                 Status: ${myGame.status} <br> 
+                                 Hours: ${myGame.hours}`;
+    fetch('http://localhost:8080')
+        
+        .then(response => response.text())
+        .then(text => console.log(text))
 }
 
-var myGame = new Game("Minecraft", "Want to play");
+var myGame = new Game("Minecraft", "Want to play", "35");
 var gameList = [myGame];
 
 // Get the paragraph element by its ID
@@ -28,4 +38,5 @@ var gameInfoElement = document.getElementById("gameInfo");
 
 // Display the game information in the paragraph
 gameInfoElement.innerHTML = `Title: ${myGame.title} <br> 
-                             Status: ${myGame.status} <br>`;
+                            Status: ${myGame.status} <br> 
+                            Hours: ${myGame.hours}`;
